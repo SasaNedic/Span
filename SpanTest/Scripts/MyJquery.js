4dfaec557b009c03dcb97ed11517a502f4fd7ac4
Saša Nedić <sasa.morpheus.nedic@gmail.com>
2015-04-16 14:05:10 +0200

    Names and dates
    Multiple names and dates

--- SpanTest/Scripts/MyJquery.js
+++ SpanTest/Scripts/MyJquery.js
@@ -1,35 +1,104 @@
-﻿$(document).ready(function () {
+﻿var nameCounter = 1;
+var dateCounter = 1;
+$(document).ready(function () {
 
+
+    /*----- add textboxes for dates -----*/
+    $("#addAccommodationDayButton").click(function () {
+        /*  ADD max i min date iz #to i #from
+        if (dateCounter > 10) {
+        alert("Možete");
+        return false;
+        }
+        */
+        dateCounter++;
+
+        var newTextBoxDiv = $(document.createElement('div'))
+	     .attr({ id: 'DateTextBoxDiv' + dateCounter, class: 'NamesClass' });
+
+        newTextBoxDiv.after().html('<label>Datum: </label>' +
+	      '<input type="text" name="datebox' + dateCounter +
+	      '" id="datebox' + dateCounter + '"readonly="readonly" class="Date"/>');
+
+        newTextBoxDiv.appendTo("#DatesGroup");
+    });
+
+    $("#removeAccommodationDayButton").click(function () {
+        if (dateCounter == 0) {
+            alert("Nema više datuma za brisanje");
+            return false;
+        }
+
+        $("#DateTextBoxDiv" + dateCounter).remove();
+
+        dateCounter--;
+    });
+    /*----- add textboxes for names -----*/
+    $("#addPersonButton").click(function () {
+
+        if (nameCounter > 10) {
+            alert("Možete tražiti putne naloge samo za 10 ljudi");
+            return false;
+        }
+        nameCounter++;
+
+        var newTextBoxDiv = $(document.createElement('div'))
+	     .attr({ id: 'NamesTextBoxDiv' + nameCounter, class: 'NamesClass' });
+
+        newTextBoxDiv.after().html('<label>Name: </label>' +
+	      '<input type="text" name="textbox' + nameCounter +
+	      '" id="textbox' + nameCounter + '" value=""/><label> Student:</label><input type="checkbox" name="checkboxStudent' +
+          nameCounter + '" id="checkboxStudent' + nameCounter + '"/><label> Suvozač:</label><input type="checkbox" name="checkboxPassenger' +
+          nameCounter + '" id="checkboxPassenger' + nameCounter + '"/>');
+
+        newTextBoxDiv.appendTo("#NamesGroup");
+    });
+
+    $("#removePersonButton").click(function () {
+        if (nameCounter == 1) {
+            alert("Mora postojati barem jedna osoba koja ide na put");
+            return false;
+        }
+
+        $("#TextBoxDiv" + nameCounter).remove();
+
+        nameCounter--;
+    });
+
+    /*----- datepickers -----*/
     $("#from").datepicker({
-        dateFormat: 'dd/mm/yy',
+        dateFormat: "dd/mm/yy",
         defaultDate: "+1w",
         onClose: function (selectedDate) {
             $("#to").datepicker("option", "minDate", selectedDate);
         }
     });
     $("#to").datepicker({
-        dateFormat: 'dd/mm/yy',
+        dateFormat: "dd/mm/yy",
         defaultDate: "+1w",
         onClose: function (selectedDate) {
             $("#from").datepicker("option", "maxDate", selectedDate);
         }
     });
 
-    $(".Date").datepicker({
-        dateFormat: 'dd/mm/yy'
+    $(document).on("focus", ".Date", function () {
+        $(this).datepicker({
+            dateFormat: "dd/mm/yy"
+        });
     });
 
-    $('#AccommodationCheckbox').change(function () {
+    /*----- show if needed -----*/
+    $("#AccommodationCheckbox").change(function () {
         if (this.checked) {
-            $('.accomodation-label').show();
-            $('.accomodation-field').show();
+            $(".accommodation-label").show();
+            $(".accommodation-field").show();
+            $(".accommodation-button").show();
         }
         else {
-            $('.accomodation-label').hide();
-            $('.accomodation-field').hide();
+            $(".accommodation-label").hide();
+            $(".accommodation-field").hide();
+            $(".accommodation-button").hide();
         }
     });
-
-
 });
 
